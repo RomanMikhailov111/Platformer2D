7cfadaf8d563cd06095c8377ed1e8f86729ab551
RomanMikhailov <chap9iduk@gmail.com>
2024-06-22 11:18:11 +0300

    ADDReloadScene

---
+++ Assets/ReloadLevelComponent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets
+{
+    public class ReloadLevelComponent : MonoBehaviour
+    {
+        public void ReloadLevel()
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+
+       
+    }
+}
